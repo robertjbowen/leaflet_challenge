f4c60f32e975ddb961b5cfba2fdcd6df48767d13
robertjbowen <69124118+robertjbowen@users.noreply.github.com>
2021-01-26 22:08:37 -0600

    initialized logic.js and confid.js added .gitignore

---
+++ Leaflet-Step-1/static/js/logic.js
@@ -0,0 +1,51 @@
+// Creating map object
+var myMap = L.map("mapid", {
+  center: [40, -90],
+  zoom: 5
+});
+
+// Adding tile layer to the map
+L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+  attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+  tileSize: 512,
+  maxZoom: 18,
+  zoomOffset: -1,
+  id: "mapbox/streets-v11",
+  accessToken: API_KEY
+}).addTo(myMap);
+
+// Store API query variables
+var baseURL = "https://earthquake.usgs.gov/earthquakes/feed/v1.0";
+var method = "/summary";
+var parameters = "/all_month.geojson"; // "/all_week.geojson";
+
+// Assemble API query URL
+var url = baseURL + method + parameters;
+
+// Grab the data with d3
+d3.json(url, function(response) {
+  console.log(response);
+
+  // Create a new marker cluster group
+  var markers = L.markerClusterGroup();
+
+  // Loop through data
+  for (var i = 0; i < response.length; i++) {
+
+    // Set the data location property to a variable
+    var location = response[i].location;
+
+    // Check for location property
+    if (location) {
+
+      // Add a new marker to the cluster group and bind a pop-up
+      markers.addLayer(L.marker([location.coordinates[1], location.coordinates[0]])
+        .bindPopup(response[i].descriptor));
+    }
+
+  }
+
+  // Add our marker cluster layer to the map
+  myMap.addLayer(markers);
+
+});
